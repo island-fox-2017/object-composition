8d56deb01308ed2cd6c54fdea84eb51d1fcebfe1
anto <irianto223@gmail.com>
2017-07-06 12:35:44 +0700

    olah ingredients jadi arr of object

--- composition.js
+++ composition.js
@@ -3,10 +3,11 @@ const fs = require('fs')
 var options = fs.readFileSync('cookies.txt').toString().trim().split('\n')
 
 class Cookie {
-  constructor(name) {
+  constructor(name, ingred, sugar) {
     this.name = name
     this.status = 'mentah'
-    this.ingredients = []
+    this.ingredients = ingred
+    this.has_sugar = sugar
   }
 
   bake() {
@@ -15,48 +16,89 @@ class Cookie {
 }
 
 class PeanutButter extends Cookie {
-  constructor(name) {
-    super(name)
+  constructor(name, ingred, sugar) {
+    super(name, ingred, sugar)
     this.peanut_count = 100
   }
 }
 
 class ChocholateChip extends Cookie {
-  constructor(name) {
-    super(name)
+  constructor(name,ingred,sugar) {
+    super(name, ingred,sugar)
     this.choc_chip_count = 200
   }
 }
 
 class OtherCookie extends Cookie {
-  constructor(name) {
-    super(name)
+  constructor(name,ingred,sugar) {
+    super(name, ingred,sugar)
     this.other_cookie_count = 150
   }
 }
 
 class CookieFactory {
-  constructor() {
-
-  }
+  constructor() {}
 
   static create(options) {
-    let arrCookie = []
+    let arr = []
     for (let i=0; i<options.length; i++) {
-      if (options[i] == 'peanut butter') {
-        arrCookie.push(new PeanutButter(options[i]))
+
+      if (options[i].split(' = ')[0] == 'peanut butter') {
+        arr.push(new PeanutButter(options[i].split(' = ')[0], this.olahIngredients(i), this.cekGula(i)))
       }
-      else if (options[i] == 'chocolate chip') {
-        arrCookie.push(new ChocholateChip(options[i]))
+      else if (options[i].split(' = ')[0] == 'chocolate chip') {
+        arr.push(new ChocholateChip(options[i].split(' = ')[0], this.olahIngredients(i), this.cekGula(i)))
       }
       else {
-        arrCookie.push(new OtherCookie('Other Cookie'))
+        arr.push(new OtherCookie('other cookie', this.olahIngredients(i), this.cekGula(i)))
       }
     }
-    return arrCookie
+    return arr
+  }
+
+  static olahIngredients(cookieKe) { //olah ingredients jadi array of object
+    let listObjIngredients = []
+    let listIngredients = options[cookieKe].split(' = ')[1].split(', ')
+    for (let i=0; i<listIngredients.length; i++) {
+      let arrAmountBahan = listIngredients[i].split(': ')
+      listObjIngredients.push(JSON.stringify(new Ingredient(arrAmountBahan)))
+    }
+    return listObjIngredients
+  }
+
+  static cekGula(bahanCookieKe) {
+    if (/sugar/g.test(options[bahanCookieKe])) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  static cookieRecomendation(day,listCookie) {
+    let noSugarCookie = []
+    if (day == 'tuesday') {
+      for (let i=0; i<listCookie.length; i++) {
+        if (listCookie[i].has_sugar == false) {
+          noSugarCookie.push(listCookie[i])
+        }
+      }
+    }
+    return noSugarCookie
   }
 }
 
+class Ingredient {
+  constructor(arrBahan) {
+    this.name = arrBahan[1]
+    this.amount = arrBahan[0]
+  }
+}
 
 let batch_of_cookies = CookieFactory.create(options)
-console.log(batch_of_cookies);
+// console.log(batch_of_cookies);
+let sugarFreeFoods = CookieFactory.cookieRecomendation('tuesday',batch_of_cookies)
+console.log('sugar free cakes are: ');
+for (let i=0; i<sugarFreeFoods.length; i++) {
+  console.log(sugarFreeFoods[i]);
+}
